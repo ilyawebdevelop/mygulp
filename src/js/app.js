44e53a537220233e8d7d9dc3faa2f82daa2969ca
ilyawebdevelop <ilia966@tut.by>
2023-12-11 13:38:02 +0300

    first

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+import * as flsFunctions from "./modules/functions.js";
+import "./modules/jquery-3.7.1.min.js";
+import "./modules/bootstrap.bundle.min.js";
+import './components.js';
+
+flsFunctions.isWebp();
+
+// Import swiper
+import Swiper, { Navigation, Pagination, Autoplay, Mousewheel, EffectFade, Thumbs, Scrollbar } from 'swiper';
+Swiper.use([Navigation, Pagination, Autoplay, Mousewheel, EffectFade, Thumbs, Scrollbar]);
+
+
+
+
